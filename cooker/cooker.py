0e02ee3ba293d66496389c8e566943da2a8d8f3f
Christophe Blaess <christophe.blaess@logilin.fr>
2022-02-11 00:43:41 +0100

    [DEV] Read `CONF_VERSION` from `local.conf.sample` file.
    The path to this file is in `templateconf.cfg`, relative to `poky`
    directory (or `openembedded-core` for Arago-based systems).
    
    If the file is missing (for example when running with `--dry-run`)
    we use the `DEFAULT_CONF_VERSION` from the Distro config.

--- cooker/cooker.py
+++ cooker/cooker.py
@@ -296,21 +296,23 @@ def resolve_parents():
 class PokyDistro:
 
     DISTRO_NAME = "poky"
-    BUILD_SCRIPT = "poky/oe-init-build-env"
+    BASE_DIRECTORY = "poky"
+    BUILD_SCRIPT = "oe-init-build-env"
     TEMPLATE_CONF = "meta-poky/conf"
+    DEFAULT_CONF_VERSION = "1"
     LAYER_CONF_NAME = "POKY_BBLAYERS_CONF_VERSION"
     LAYER_CONF_VERSION = "2"
-    LOCAL_CONF_VERSION = "1"
     PACKAGE_FORMAT = "package_rpm"
 
 class AragoDistro:
 
     DISTRO_NAME = "arago"
-    BUILD_SCRIPT = "openembedded-core/oe-init-build-env"
+    BASE_DIRECTORY = "openembedded-core"
+    BUILD_SCRIPT = "oe-init-build-env"
     TEMPLATE_CONF = "meta/conf"
+    DEFAULT_CONF_VERSION = "1"
     LAYER_CONF_NAME = "LCONF_VERSION"
     LAYER_CONF_VERSION = "7"
-    LOCAL_CONF_VERSION = "1"
     PACKAGE_FORMAT = "package_ipk"
 
 
@@ -444,9 +446,23 @@ class CookerCommands:
     def generate(self):
         info('Generating dirs for all build-configurations')
 
+        self.read_local_conf_version()
+
         for build in BuildConfiguration.ALL.values():
             if build.buildable():
                 self.prepare_build_directory(build)
+
+    def read_local_conf_version(self):
+        try:
+            self.distro.local_conf_version = "{}".format(self.distro.DEFAULT_CONF_VERSION)
+            file = open(self.config.layer_dir() + self.distro.BASE_DIRECTORY + "/" + self.distro.TEMPLATE_CONF + "/local.conf.sample")
+            for line in file.readlines():
+                if line.lstrip().startswith("CONF_VERSION"):
+                    idx = line.rfind("=")
+                    self.distro.local_conf_version = int(line[idx+1:].replace('"', ''))
+                    return
+        except:
+            return
 
     def prepare_build_directory(self, build):
         debug('Preparing directory:', build.dir())
@@ -480,7 +496,7 @@ class CookerCommands:
         CookerCall.os.file_write(file, '\tABORT,${DL_DIR},100M,1K \\')
         CookerCall.os.file_write(file, '\tABORT,${SSTATE_DIR},100M,1K \\')
         CookerCall.os.file_write(file, '\tABORT,/tmp,10M,1K"')
-        CookerCall.os.file_write(file, 'CONF_VERSION = "{}"'.format(self.distro.LOCAL_CONF_VERSION))
+        CookerCall.os.file_write(file, 'CONF_VERSION ?= "{}"'.format(self.distro.local_conf_version))
         CookerCall.os.file_close(file)
 
         file = CookerCall.os.file_open(os.path.join(conf_path, 'bblayers.conf'))
@@ -541,7 +557,7 @@ class CookerCommands:
             if build_arg:
                 if build.targets():
                     info('  .',
-                         os.path.relpath(self.config.layer_dir(self.distro.BUILD_SCRIPT), os.getcwd()),
+                         os.path.relpath(self.config.layer_dir(self.distro.BASE_DIRECTORY + "/" + self.distro.BUILD_SCRIPT), os.getcwd()),
                          os.path.relpath(build.dir(), os.getcwd()))
                 else:
                     info('build', build.name(), 'has no target')
@@ -608,7 +624,7 @@ class CookerCommands:
     def run_bitbake(self, build_config, bb_task, bb_target):
         directory = build_config.dir()
 
-        init_script = self.config.layer_dir(self.distro.BUILD_SCRIPT)
+        init_script = self.config.layer_dir(self.distro.BASE_DIRECTORY + "/" + self.distro.BUILD_SCRIPT)
         if not CookerCall.os.file_exists(init_script):
             fatal_error('init-script', init_script, 'not found')
 
@@ -619,7 +635,7 @@ class CookerCommands:
 
     def shell(self, build_names: List[str]):
         build_dir = self.get_buildable_builds(build_names)[0].dir()
-        init_script = self.config.layer_dir(self.distro.BUILD_SCRIPT)
+        init_script = self.config.layer_dir(self.distro.BASE_DIRECTORY + "/" + self.distro.BUILD_SCRIPT)
         shell = os.environ.get('SHELL', '/bin/sh')
 
         debug('running interactive, poky-initialized shell {} {} {}', build_dir, init_script, shell)
