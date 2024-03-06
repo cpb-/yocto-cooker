a0bcb1a3245e381db58f9e55674e5c1e6c91acfb
Pierre-loup Gosse <pierre-loup.gosse@agregio-solutions.com>
2024-03-06 17:32:02 +0100

    feat: add log output format

--- cooker/cooker.py
+++ cooker/cooker.py
@@ -12,6 +12,7 @@ import jsonschema
 import pkg_resources
 import subprocess
 import shlex
+from abc import ABC, abstractmethod
 
 from typing import List
 
@@ -373,6 +374,105 @@ class AragoDistro:
     BITBAKE_INIT_FILE = "sources/bitbake/lib/__init__.py"
 
 
+class LogFormat(ABC):
+
+    def __init__(self, changes):
+        self.changes = changes
+        self.output = ""
+
+    @abstractmethod
+    def print_history(self, history):
+        pass
+
+    @abstractmethod
+    def print_added_item(self, source, rev):
+        pass
+
+    def print_modified_item(self, source, data):
+        if 'history' in data:
+            self.print_history(data['history'])
+
+    @abstractmethod
+    def print_deleted_item(self, source, rev):
+        pass
+
+    def print_added(self, changes):
+        for source, data in changes.items():
+            self.print_added_item(source, data)
+
+    def print_modified(self, changes):
+        for source, data in changes.items():
+            self.print_modified_item(source, data)
+
+    def print_deleted(self, changes):
+        for source, data in changes.items():
+            self.print_deleted_item(source, data)
+
+    def generate(self):
+        if len(self.changes['added']):
+            self.print_added(self.changes['added'])
+
+        if len(self.changes['modified']):
+            self.print_modified(self.changes['modified'])
+
+        if len(self.changes['deleted']):
+            self.print_deleted(self.changes['deleted'])
+
+    def add_line(self, l=""):
+        if self.output:
+            self.output += "\n"
+        self.output += l
+
+    def display(self):
+        print(self.output)
+
+class LogTextFormat(LogFormat):
+
+    def print_history(self, history):
+        for line in history:
+            self.add_line('  {}'.format(line))
+
+    def print_added_item(self, source, rev):
+        self.add_line('A {}: {}'.format(source, rev))
+
+    def print_modified_item(self, source, data):
+        self.add_line('M {}: {} .. {}'.format(source, data['from'], data['to']))
+        super().print_modified_item(source, data)
+
+    def print_deleted_item(self, source, rev):
+        self.add_line('D {}: {}'.format(source, rev))
+
+class LogMarkdownFormat(LogFormat):
+
+    def print_history(self, history):
+        for line in history:
+            self.add_line('  - {}'.format(line))
+
+    def print_added_item(self, source, rev):
+        self.add_line('- {} at revision {}'.format(source, rev))
+
+    def print_modified_item(self, source, data):
+        self.add_line('- {} changed from {} to {}'.format(source, data['from'], data['to']))
+        super().print_modified_item(source, data)
+
+    def print_deleted_item(self, source, rev):
+        self.add_line('- {} at revision {}'.format(source, rev))
+
+    def print_added(self, changes):
+        self.add_line('## Added projects')
+        super().print_added(changes)
+        self.add_line()
+
+    def print_modified(self, changes):
+        self.add_line('## Modified projects')
+        super().print_modified(changes)
+        self.add_line()
+
+    def print_deleted(self, changes):
+        self.add_line('## Deleted projects')
+        super().print_deleted(changes)
+        self.add_line()
+
 class CookerCommands:
     """ The class aggregates all functions representing a low-level cooker-command """
 
@@ -654,6 +754,16 @@ class CookerCommands:
                         debug(complete.stderr.decode('ascii'))
                         continue
                     data['history'] = complete.stdout.decode('ascii').splitlines()
+
+        # Prints the formatted log output from the changes dict.
+
+        if log_format == 'md' or log_format == 'markdown':
+            log = LogMarkdownFormat(changes)
+        else:
+            log = LogTextFormat(changes)
+
+        log.generate()
+        log.display()
 
     def generate(self):
         info('Generating dirs for all build-configurations')
