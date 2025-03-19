ea93cd0de82394c7b8d4995c1aea6ec972fea006
Marc-Hendric Lühr <168063227+MarcHLuehr@users.noreply.github.com>
2025-03-19 08:23:29 +0100

    Add linting and format (#155)
    * feat: Add ruff and mypy checks
    
    * chore: Format files
    
    * fix: Correct minor linting finds
    
    * feat: working on lint
    
    * feat: Fix mypy issues

---
+++ cooker/distro.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+
+class Distro:
+    DISTRO_NAME: str
+    BASE_DIRECTORY: str
+    BUILD_SCRIPT: str
+    TEMPLATE_CONF: Tuple[str, ...]
+    DEFAULT_CONF_VERSION: str
+    LAYER_CONF_NAME: str
+    LAYER_CONF_VERSION: str
+    PACKAGE_FORMAT: str
+    DEFAULT_BITBAKE_MAJOR_VERSION: int
+    BITBAKE_INIT_FILE: str
+
+
+class PokyDistro(Distro):
+    DISTRO_NAME = "poky"
+    BASE_DIRECTORY = "poky"
+    BUILD_SCRIPT = "oe-init-build-env"
+    TEMPLATE_CONF = ("meta-poky/conf", "meta-poky/conf/templates/default")
+    DEFAULT_CONF_VERSION = "1"
+    LAYER_CONF_NAME = "POKY_BBLAYERS_CONF_VERSION"
+    LAYER_CONF_VERSION = "2"
+    PACKAGE_FORMAT = "package_rpm"
+    DEFAULT_BITBAKE_MAJOR_VERSION = 2
+    BITBAKE_INIT_FILE = "bitbake/lib/bb/__init__.py"
+
+
+class AragoDistro(Distro):
+    DISTRO_NAME = "arago"
+    BASE_DIRECTORY = "openembedded-core"
+    BUILD_SCRIPT = "oe-init-build-env"
+    TEMPLATE_CONF = ("meta/conf",)
+    DEFAULT_CONF_VERSION = "1"
+    LAYER_CONF_NAME = "LCONF_VERSION"
+    LAYER_CONF_VERSION = "7"
+    PACKAGE_FORMAT = "package_ipk"
+    DEFAULT_BITBAKE_MAJOR_VERSION = 2
+    BITBAKE_INIT_FILE = "sources/bitbake/lib/__init__.py"
--- setup.py
+++ setup.py
@@ -1,49 +1,41 @@
-__title__ = 'yocto-cooker'
-__version__ = '1.4.0'
+__title__ = "yocto-cooker"
+__version__ = "1.4.0"
 __author__ = "Christophe BLAESS, Patrick BOETTCHER"
-__email__ = 'christophe.blaess@logilin.fr, p@yai.se'
+__email__ = "christophe.blaess@logilin.fr, p@yai.se"
 __license__ = "GPL"
 __url__ = "https://github.com/cpb-/yocto-cooker"
 
-from setuptools import setup
 import os
 
+from setuptools import setup
 
 # Get description from the README file
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
     _long_description = f.read()
 
 
 setup(
     name=__title__,
     version=__version__,
-    description='meta build tool for Yocto Project based Linux embedded systems',
+    description="meta build tool for Yocto Project based Linux embedded systems",
     author=__author__,
     author_email=__email__,
     url=__url__,
     license=__license__,
     long_description=_long_description,
-    long_description_content_type='text/markdown',
-    package_data={'': ['cooker-menu-schema.json']},
-
+    long_description_content_type="text/markdown",
+    package_data={"": ["cooker-menu-schema.json"]},
     packages=[
         "cooker",
     ],
-
     entry_points={
-        'console_scripts': [
-            'cooker = cooker.cooker:main',
+        "console_scripts": [
+            "cooker = cooker.cooker:main",
         ],
     },
-
-    install_requires=[
-        "jsonschema >= 3.2.0",
-        "urllib3 >= 1.22",
-        "pyjson5 >= 1.6.2"
-    ],
-
+    install_requires=["jsonschema >= 3.2.0", "urllib3 >= 1.22", "pyjson5 >= 1.6.2"],
     project_urls={
-        'Bug Reports': 'https://github.com/cpb-/yocto-cooker/issues',
-        'Source': __url__,
+        "Bug Reports": "https://github.com/cpb-/yocto-cooker/issues",
+        "Source": __url__,
     },
 )
